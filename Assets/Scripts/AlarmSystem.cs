879c1dee2d5ddb67a2e322e6fd69a8ee9e27afe4
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-11-14 09:07:17 +0300

    Refactoring AlarmSystem -> add class Trigger.

--- Assets/Scripts/AlarmSystem.cs
+++ Assets/Scripts/AlarmSystem.cs
@@ -5,39 +5,11 @@ using UnityEngine;
 public class AlarmSystem : MonoBehaviour
 {
     private AudioSource _audioSource;
-    private Coroutine _coroutineAlarmOn;
-    private Coroutine _coroutineAlarmOff;
     private int _speedVolume = 2;
     
     private void Start() => _audioSource = GetComponent<AudioSource>();
 
-    private void OnTriggerEnter2D(Collider2D collider2D)
-    {
-        if (collider2D.TryGetComponent(out Thief thief))
-        {
-            if (_coroutineAlarmOff != null)
-            {
-                StopCoroutine(_coroutineAlarmOff);
-            }
-            
-            _coroutineAlarmOn = StartCoroutine(AlarmOn());
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collider2D)
-    {
-        if (collider2D.TryGetComponent(out Thief thief))
-        {
-            if (_coroutineAlarmOn != null)
-            {
-                StopCoroutine(_coroutineAlarmOn);
-            }
-            
-            _coroutineAlarmOff = StartCoroutine(AlarmOff());
-        }
-    }
-
-    private IEnumerator AlarmOn()
+    public IEnumerator AlarmOn()
     {
         _audioSource.Play();
         
@@ -49,7 +21,7 @@ public class AlarmSystem : MonoBehaviour
         }
     }
     
-    private IEnumerator AlarmOff()
+    public IEnumerator AlarmOff()
     {
         while (_audioSource.volume > 0)
         {
@@ -60,4 +32,6 @@ public class AlarmSystem : MonoBehaviour
         
         _audioSource.Stop();
     }
+    
+    
 }
