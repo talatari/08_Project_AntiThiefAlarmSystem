fa6e41e23bce17520e5118a86ddf3d38a5ad80ee
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-11-23 12:07:27 +0300

    Refactoring codes. See comment.

--- Assets/Scripts/ThiefDetector.cs
+++ Assets/Scripts/ThiefDetector.cs
@@ -3,25 +3,20 @@ using UnityEngine;
 
 public class ThiefDetector : MonoBehaviour
 {
-    [SerializeField] private VolumeControl _volumeControl;
-
     private int _directionVolume = -1;
     
     public event Action<int> ThiefEnter;
     public event Action<int> ThiefExit;
     
-    private void Awake() => 
-        _volumeControl = FindObjectOfType<VolumeControl>();
-
-    private void OnTriggerEnter2D(Collider2D collider2D)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collider2D.TryGetComponent(out Thief thief)) 
+        if (other.TryGetComponent(out Thief thief)) 
             ThiefEnter?.Invoke(_directionVolume * _directionVolume);
     }
 
-    private void OnTriggerExit2D(Collider2D collider2D)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if (collider2D.TryGetComponent(out Thief thief)) 
+        if (other.TryGetComponent(out Thief thief)) 
             ThiefExit?.Invoke(_directionVolume);
     }
 }
